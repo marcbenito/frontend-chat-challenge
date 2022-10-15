5ac3f0e70e250c894c4e0cb26b685f4403644d41
Marc Benito <marcbenito@gmail.com>
2022-10-15 13:01:18 +0200

    feat: initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import Chat from './Chat';
+
+export default function App() {
+  return (
+    <section id="landbot-app" className="hero is-fullheight is-dark">
+      <div className="hero-body">
+        <div className="chat-container">
+          <div className="landbot-chat">
+            <Chat />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
